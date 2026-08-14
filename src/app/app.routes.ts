f472f334ee0386e000166f90dab3cf1839799867
Yasmin Vitória da Silva Sobrinho <ysobrinho79@gmail.com>
2026-08-13 22:21:56 -0300

    Projeto "cafeteria"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home/home').then((m) => m.Home)
+  },
+  {
+    path: 'cardapio',
+    loadComponent: () =>
+      import('./features/cardapio/cardapio/cardapio').then((m) => m.Cardapio)
+  },
+  {
+    path: 'carrinho',
+    loadComponent: () =>
+      import('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/login/login/login').then((m) => m.Login)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
---
+++ src/app/features/carrinho/carrinho/carrinho.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [],
+  templateUrl: './carrinho.html',
+  styleUrl: './carrinho.css',
+})
+export class Carrinho {}
