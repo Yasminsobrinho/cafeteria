0cfacafddb6be56dc891cd290f84046710c09ab6
emilly vitoria <emillyviviti@gmail.com>
2026-08-27 11:36:10 -0300

    avaliações

--- src/app/features/home/home/home.ts
+++ src/app/features/home/home/home.ts
@@ -1,33 +1,85 @@
+
 // Importa a classe Component para criar um componente Angular
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 // Importa RouterLink para permitir a navegação entre as páginas
 import { RouterLink } from '@angular/router';
-// Importa o componente Sobre
+// Importa os componentes secundários
 import { Sobre } from './pginicial/sobre/sobre';
 import { Local } from "./pginicial/local/local";
 import { Playlist } from "./pginicial/playlist/playlist";
 import { Rodape } from "./pginicial/rodape/rodape";
 
-
 // Configurações do componente Home
 @Component({
   selector: 'app-home',
   standalone: true,
-   // Importa os recursos usados no HTML
-  imports: [RouterLink, Sobre, Local, Playlist, Rodape],
+  imports: [CommonModule, RouterLink, Sobre, Local, Playlist, Rodape],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 
 export class Home {
 
   menuAberto = signal(false);
 
-   // Define o caminho/nome das imagens usadas
+  // Define o caminho/nome das imagens usadas
   logo: string = 'favicon.ico';
   imgheader: string = 'header.jpeg';
-  imglogo: string = 'logo.png'
+  imglogo: string =  'logo.png';
   imgcarrinho: string = 'carrinho.png';
+
+  // Lista de avaliações unificada na classe correta
+ avaliacoes = [
+  {
+    nome: 'Maria Silva',
+    tipo: 'Cliente',
+    estrelas: '★★★★★',
+    comentario: 'O café é maravilhoso e o atendimento é perfeito!'
+  },
+  {
+    nome: 'João Pedro',
+    tipo: 'Cliente',
+    estrelas: '★★★★★',
+    comentario: 'Os doces são deliciosos, os bolos são bem fofinhos. Com certeza voltarei!'
+  },
+  {
+    nome: 'Ana Clara',
+    tipo: 'Cliente',
+    estrelas: '★★★★★',
+    comentario: 'Ambiente muito bonito e aconchegante.'
+  },
+  {
+    nome: 'Luiz Fernando',
+    tipo: 'Cliente',
+    estrelas: '★★★★★',
+    comentario: 'As massas são super levinhas e deliciosas.'
+  },
+  {
+    nome: 'Fabrício',
+    tipo: 'Cliente',
+    estrelas: '★★★★',
+    comentario: 'Amei os doces!'
+  }
+];
+
+indiceAtual = 0;
+
+proxima() {
+  this.indiceAtual++;
+
+  if (this.indiceAtual >= this.avaliacoes.length) {
+    this.indiceAtual = 0;
+  }
+}
+
+anterior() {
+  this.indiceAtual--;
+
+  if (this.indiceAtual < 0) {
+    this.indiceAtual = this.avaliacoes.length - 1;
+  }
+}
 
   alternarMenu(): void {
     this.menuAberto.update((aberto) => !aberto);
@@ -37,5 +89,4 @@ export class Home {
     this.menuAberto.set(false);
   }
 
-  }
-
+}
