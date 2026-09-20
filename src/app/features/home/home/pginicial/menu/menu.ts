a2af953e41cae94a73028c6f27e2e7e3de0e61ec
Yasmin Vitória da Silva Sobrinho <ysobrinho79@gmail.com>
2026-09-20 20:54:37 -0300

    .

--- src/app/features/home/home/pginicial/menu/menu.ts
+++ src/app/features/home/home/pginicial/menu/menu.ts
@@ -4,19 +4,39 @@ import { CarrinhoService } from '../../../../carrinho/carrinho/carrinho.service'
 
 @Component({
   selector: 'app-menu',
+
   standalone: true,
+
   imports: [RouterLink],
+
   templateUrl: './menu.html',
+
   styleUrl: './menu.css',
 })
 export class Menu {
+  /* ==============================
+     IMAGENS
+  =============================== */
+
   imglogo: string = 'logo.png';
 
   imgcarrinho: string = 'carrinho.png';
 
+  /* ==============================
+     CONTROLE DO MENU MOBILE
+  =============================== */
+
   menuAberto = signal(false);
 
+  /* ==============================
+     CONSTRUTOR
+  =============================== */
+
   constructor(private carrinhoService: CarrinhoService) {}
+
+  /* ==============================
+     QUANTIDADE DO CARRINHO
+  =============================== */
 
   get quantidadeCarrinho(): number {
     return this.carrinhoService
@@ -24,9 +44,17 @@ export class Menu {
       .reduce((total, produto) => total + produto.quantidade, 0);
   }
 
+  /* ==============================
+     ABRIR / FECHAR MENU
+  =============================== */
+
   alternarMenu(): void {
     this.menuAberto.update((aberto) => !aberto);
   }
+
+  /* ==============================
+     FECHAR MENU
+  =============================== */
 
   fecharMenu(): void {
     this.menuAberto.set(false);
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,33 +1,21 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vitest/config';
-import angular from '@analogjs/vite-plugin-angular';
-
-export default defineConfig({
-  plugins: [angular()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./setup-vitest.ts'],
-  },
-});
-=======
 import angular from '@analogjs/vite-plugin-angular';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  root: './src',
   resolve: {
     mainFields: ['module'],
   },
   plugins: [angular()],
   optimizeDeps: {
     noDiscovery: true,
+    entries: [],
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test-setup.ts'],
-    include: ['src/**/*.spec.ts'],
-    exclude: ['dist/**'],
+    setupFiles: ['./test-setup.ts'],
+    include: ['**/*.spec.ts'],
+    exclude: ['../dist/**'],
   },
 });
->>>>>>> 30c5004c02a06f57a3a3037902da8b5d1ce9b625
